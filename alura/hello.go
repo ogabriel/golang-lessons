0fe4d649ce2b8ca79d5e186b03301cf4383bcafa
ogabriel <gabrieloliver8991@gmail.com>
2023-09-20 17:30:56 -0300

    update hello

--- alura/hello.go
+++ alura/hello.go
@@ -1,8 +1,16 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
-	fmt.Println("Olá mundo!")
-	fmt.Println("Meu primeiro código em Go!")
+	// da pra declarar váriaveis de várias formas diferentes:
+	var nome = "Gabriel"
+	var idade int = 21
+	versao := 1.1
+
+	fmt.Println("Olá, sr.", nome, "a sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
 }

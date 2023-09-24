afd263c4cc1588cf55475f887e8499acf14a35bc
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 22:18:59 -0300

    add last code of menu

--- alura/menu_adv.go
+++ alura/menu_adv.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -25,6 +26,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -84,8 +86,10 @@ func testaSite(site string) {
 
 	if resposta.StatusCode == 200 {
 		fmt.Println("Site: ", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site: ", site, "não foi carregado com sucesso, status code: ", resposta.StatusCode)
+		registraLog(site, false)
 	}
 
 }
@@ -122,4 +126,24 @@ func exibirErro(err error) {
 		fmt.Println("Ocorreu um erro", err)
 	}
 
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	exibirErro(err)
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05 - ") + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	// arquivo, err := ioutil.ReadFile("logs.txt")
+	arquivo, err := os.ReadFile("log.txt")
+
+	exibirErro(err)
+
+	conteudo := string(arquivo)
+	fmt.Println(conteudo)
 }

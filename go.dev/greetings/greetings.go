def96977cda704083a31cbd0e3557d350fccecc7
ogabriel <gabrieloliver8991@gmail.com>
2023-11-02 16:59:08 -0300

    feat: treat errors on greetings

--- go.dev/greetings/greetings.go
+++ go.dev/greetings/greetings.go
@@ -1,10 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for a named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	return message
+	return message, nil
 }

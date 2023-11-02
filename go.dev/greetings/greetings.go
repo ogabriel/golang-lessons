a64be0eb173ec89b22c4f866222b4b10acb758cb
ogabriel <gabrieloliver8991@gmail.com>
2023-11-02 16:52:13 -0300

    fix: message on greetings module

--- go.dev/greetings/greetings.go
+++ go.dev/greetings/greetings.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // Hello returns a greeting for a named person
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome", name)
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
 	return message
 }

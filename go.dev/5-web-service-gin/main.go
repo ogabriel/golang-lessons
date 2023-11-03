d97fc6c390ab5a75e54b1d659b2fad2fe6317a7c
ogabriel <gabrieloliver8991@gmail.com>
2023-11-03 13:00:36 -0300

    docs: add comments

--- go.dev/5-web-service-gin/main.go
+++ go.dev/5-web-service-gin/main.go
@@ -26,29 +26,34 @@ func main() {
 
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
-	router.GET("/albums/:id", getAlbum)
+	router.GET("/albums/:id", getAlbumByID)
 
 	router.Run("localhost:8080")
 }
 
+// getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+// postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
 
+	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
-func getAlbum(c *gin.Context) {
+func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
 	// Loop over the list of albums, looking for

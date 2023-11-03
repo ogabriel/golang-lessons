75a39d4ab1d8a3aa3f06cef3671a5837169cbf1d
ogabriel <gabrieloliver8991@gmail.com>
2023-11-03 11:59:29 -0300

    docs: add more comments

--- go.dev/4-data-access/main.go
+++ go.dev/4-data-access/main.go
@@ -9,10 +9,12 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+// Making db a global variable simplifies this example. In production, you’d avoid the global variable, such as by
+// passing the variable to functions that need it or by wrapping it in a struct.
 var db *sql.DB
 
 func main() {
-
+	// Capture connection properties.
 	cfg := mysql.Config{
 		User:   os.Getenv("DBUSER"),
 		Passwd: os.Getenv("DBPASS"),
@@ -20,9 +22,12 @@ func main() {
 		Addr:   "127.0.0.1:3306",
 		DBName: "recordings",
 	}
+	// show DSN URL
+	// fmt.Println(cfg.FormatDSN())
 
+	// Get a database handle.
 	var err error
-
+	// DSN = Data Source Name
 	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)

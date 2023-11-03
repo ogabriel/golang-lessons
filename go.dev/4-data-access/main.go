18192b81082f849219b65003770b96178ae6542f
ogabriel <gabrieloliver8991@gmail.com>
2023-11-03 12:29:20 -0300

    feat: add addAlbum

--- go.dev/4-data-access/main.go
+++ go.dev/4-data-access/main.go
@@ -57,6 +57,18 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Album found: %v\n", alb)
+
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID of added album %v\n", albID)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -105,4 +117,22 @@ func albumByID(id int64) (Album, error) {
 	}
 
 	return alb, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	return id, nil
 }

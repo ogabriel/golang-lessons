350104ace7bbccd18119c9983d1ea9a823d6f241
ogabriel <gabrieloliver8991@gmail.com>
2023-11-03 12:59:19 -0300

    feat: add GET /albums/:id

--- go.dev/5-web-service-gin/main.go
+++ go.dev/5-web-service-gin/main.go
@@ -26,6 +26,7 @@ func main() {
 
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbum)
 
 	router.Run("localhost:8080")
 }
@@ -43,4 +44,22 @@ func postAlbums(c *gin.Context) {
 
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbum(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusFound, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+
 }
